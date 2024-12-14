bdcccff10459861d1429441be7397fcf14db2170
shanmugapriyadev <108174009+shanmugapriyadev@users.noreply.github.com>
2024-12-14 23:45:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    darkModeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+});
